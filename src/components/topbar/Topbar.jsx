29e57a44f7c086c1e27953f30a43bb8b6b4ee86a
frederick486 <frederick.gaittet@orange.fr>
2023-03-30 00:48:35 +0200

    animation <Topbar/>

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,16 +1,28 @@
 import './topbar.scss'
+import Mail from '@mui/icons-material/Mail';
+import Person from '@mui/icons-material/Person';
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className='topbar'>
+    <div className={'topbar ' + (menuOpen && 'active')}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className='logo'>genius.</a>
           <div className="itemContainer">
-            
+            <Person className='icon'/>
+            <span>+ 06 18 53 08 59</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className='icon'/>
+            <span>frederick.gaittet@orange.fr</span>
           </div>
         </div>
         <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
       </div>      
     </div>
